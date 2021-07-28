de36d531ab74fe5dbf723faa864a6c5aa2329b2d
Xieu Xay Thit <mr.khas@gmail.com>
2021-07-28 15:12:38 +0700

    update all

--- truffle-config.js
+++ truffle-config.js
@@ -23,7 +23,7 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = fs.readFileSync(".secret").toString().trim(); // .secret file contains wallet mnemonic
 
 module.exports = {
   /**
