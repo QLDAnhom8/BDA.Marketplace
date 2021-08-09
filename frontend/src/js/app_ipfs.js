d15a872901396815c49c91f94c884b0216bbf57b
Xieu Xay Thit <mr.khas@gmail.com>
2021-08-09 11:11:40 +0700

    Demo upload file to IPFS

--- frontend/src/js/app_ipfs.js
+++ frontend/src/js/app_ipfs.js
@@ -2,6 +2,8 @@ AppIPFS = {
     IpfsNode: null,
     selectedFile: "",
     selectedFileCID: null,
+    selectedFileUploaded: false,
+    selectedJSON: null,
 
     init: async () => {
         if(AppIPFS.IpfsNode === null || AppIPFS.IpfsNode === undefined) {
